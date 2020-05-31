8befbee3ce0032f1309ab1c2079c0b0fce4fd213
Masato Ikeda <masato.ikeda@gmail.com>
2020-05-31 21:48:12 +0900

    hack

--- debug/4-0.rb
+++ debug/4-0.rb
@@ -1,77 +1,15 @@
 module Sprockets
-  class Manifest
-    def find(*args)
-      unless environment
-        raise Error, "manifest requires environment for compilation"
-      end
-
-      return to_enum(__method__, *args) unless block_given?
-
-      environment = self.environment.cached
-      promises = args.flatten.map do |path|
-        Concurrent::Promise.execute(executor: executor) do
-          #binding.pry
-          environment.find_all_linked_assets(path) do |asset|
-            yield asset
-          end
-        end
-      end
-      promises.each(&:wait!)
-
-      nil
+  module Transformers
+    def expand_transform_accepts_with_debug(parsed_accepts)
+      expand_transform_accepts_without_debug(parsed_accepts) + [
+        ["text/eco", 0.8],
+        ["text/eco+ruby", 0.8],
+        ["application/javascript+function", 0.8],
+        ["application/javascript", 0.8],
+      ]
     end
 
-    def compile(*args)
-      unless environment
-        raise Error, "manifest requires environment for compilation"
-      end
-
-      filenames            = []
-      concurrent_exporters = []
-
-      assets_to_export = Concurrent::Array.new
-      find(*args) do |asset|
-        assets_to_export << asset
-      end
-
-      p args
-      p assets_to_export
-      assets_to_export.each do |asset|
-        mtime = Time.now.iso8601
-        files[asset.digest_path] = {
-          'logical_path' => asset.logical_path,
-          'mtime'        => mtime,
-          'size'         => asset.bytesize,
-          'digest'       => asset.hexdigest,
-
-          # Deprecated: Remove beta integrity attribute in next release.
-          # Callers should DigestUtils.hexdigest_integrity_uri to compute the
-          # digest themselves.
-          'integrity'    => DigestUtils.hexdigest_integrity_uri(asset.hexdigest)
-        }
-        assets[asset.logical_path] = asset.digest_path
-
-        filenames << asset.filename
-
-        promise      = nil
-        exporters_for_asset(asset) do |exporter|
-          next if exporter.skip?(logger)
-
-          if promise.nil?
-            promise = Concurrent::Promise.new(executor: executor) { exporter.call }
-            concurrent_exporters << promise.execute
-          else
-            concurrent_exporters << promise.then { exporter.call }
-          end
-        end
-      end
-
-      # make sure all exporters have finished before returning the main thread
-      concurrent_exporters.each(&:wait!)
-      save
-
-      p filenames
-      filenames
-    end
+    alias expand_transform_accepts_without_debug expand_transform_accepts
+    alias expand_transform_accepts expand_transform_accepts_with_debug
   end
 end
